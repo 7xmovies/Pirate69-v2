d48292a7832340559d38b0552d2b282c192692b5
7xmovies <7xmovieshub@gmail.com>
2026-09-09 09:30:07 +0000

    Remove test files

--- test_render_sim.cjs
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-async function run() {
-  try {
-      const res = await axios.get(`https://pirate69-nk10.onrender.com/api/search?page=1&source=rogmovies`, { maxRedirects: 0, validateStatus: () => true });
-      console.log(res.data);
-  } catch(e) { console.error(e) }
-}
-run();
